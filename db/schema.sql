344eb72fed1f49e4913f1ad2b90179772959bd53
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-06-01 12:38:58 -0400

    adding tables

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,30 @@ CREATE TABLE department (
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT DEFAULT NULL,
+    manager_id INT,
+
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL    
+);
+
